953a2df47a9f117d0fce76978195b543f2a5f58d
renato <7tkmjuewgdv3ask3jnkjpy5oqnnrzt3qm7v2f55pooiml2hwxhtq>
2021-09-20 16:39:29 -0300

    Something changed

--- stories/CollectiveByPresence/ElementExample/ElementExample.runscript.js
+++ stories/CollectiveByPresence/ElementExample/ElementExample.runscript.js
@@ -15,3 +15,111 @@ function init (sectionDomId) {
 }
 
 init()
+
+$(document).ready(() => {
+    /**
+     * Api
+     */
+    const url1 = 'https://gw.luxurypresence.com';
+    const companyId1 = '';
+    const propertyTypes1 = `
+    query PropertyTypes {
+      propertyTypes {
+        name
+      }
+    }
+  `;
+
+    const locations1 = `
+    query Neighborhoods(
+    $search: String, 
+    $companyId: String, 
+    $limit: Int = 2
+    ) {
+      neighborhoods(
+      search: $search, 
+      companyId: $companyId, 
+      limit: $limit
+      ) {
+        id
+        name
+      }
+    }
+  `;
+
+    const featuredProperties1 = `
+    query Properties(
+      $search: String, 
+      $featuredListing: Boolean = true,
+      $companyId: String, 
+      $limit: Int = 2
+    ) {
+      properties(
+        search: $search, 
+        featuredListing: $featuredListing,
+        companyId: $companyId, 
+        limit: $limit
+      ) {
+        id
+        name
+      }
+    }
+  `;
+
+    const fetchResults1 = (params) => {
+        return superagent
+            .post(url1 + '/graphql')
+            .withCredentials()
+            .send({
+                query: params.query,
+                variables: {
+                    companyId: companyId1,
+                    search: params.search
+                }
+            })
+    }
+
+    const onSearch = (e) => {
+        const search = e.target.value;
+        if (e.length <= 1) return;
+
+        const predefinedParams = {
+            companyId1,
+            search
+        }
+
+        Promise.all([
+            fetchResults1({
+                query: locations1,
+                companyId1,
+                search
+            }),
+            fetchResults1({
+                query: featuredProperties1,
+                companyId1,
+                search
+            }),
+            fetchResults1({
+                query: propertyTypes1,
+                companyId1,
+                search
+            })
+        ])
+            .then(res => {
+                const items = {};
+                items.locations = res[0].body.data.neighborhoods;
+                items.featuredProperties = res[1].body.data.properties;
+                items.propertyTypes = res[2].body.data.propertyTypes
+                    .filter(item =>
+                        item.name.toLowerCase().includes(search.toLowerCase())
+                    )
+                    .map((item, index) => ({
+                        id: index ,
+                        name: propertyTypesLabels[item.name]
+                    })).slice(0, 2);
+                // resultContainer.trigger('searchSuccess', { search, items });
+            })
+    }
+
+    onSearch({ target: { value: 12 }})
+})
